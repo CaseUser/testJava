70763e36b991316540032f7d5a163a4819308f7b
CaseUser <dear_jump@yahoo.com>
2017-09-05 16:42:33 -0400

    fix caps

--- testJava/src/testJava/newclass.java
+++ testJava/src/testJava/newclass.java
@@ -5,7 +5,7 @@ public class newclass {
 	public void test() {
 	System.out.println("tesing branch");
 	System.out.println("tesing branch");
-	System.out.println("from branch 1");
+	System.out.println("fix me");
 	}
 
 }
