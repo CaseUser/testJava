eaf36c186f5450304afe616d34e5c77c717156ba
CaseUser <dear_jump@yahoo.com>
2017-09-11 19:50:06 -0400

    Update newclass.java

--- testJava/src/testJava/newclass.java
+++ testJava/src/testJava/newclass.java
@@ -5,7 +5,7 @@ public class newclass {
 	public void test() {
 	System.out.println("tesing branch");
 	System.out.println("tesing branch");
-	System.out.println("fix me");
+	System.out.println("fix me too");
 	}
 
 }
